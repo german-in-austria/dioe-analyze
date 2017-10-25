27b3132b3c71700ae22024188a7fff77ad22b0ad
Thomas Aiglstorfer <t.aiglstorfer@gmail.com>
2017-10-25 14:13:29 +0200

    added a test component for experimental purposes

---
+++ dianalyze/src/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="test">
+    <img src="./assets/vader.jpg">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test'
+}
+</script>
+
+<style>
+#test {
+  font-family: 'Comfortaa', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- dianalyze/src/main.js
+++ dianalyze/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Test from './Test'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -12,4 +13,11 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
+})
+
+new Vue({
+  el: '#test',
+  router,
+  template: '<Test/>',
+  components: { Test }
 })
